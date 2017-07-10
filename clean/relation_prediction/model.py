79cb03b711a442850f1f53c6de0beeb6cca47666
Salman Mohammed <smohammed1993@gmail.com>
2017-07-09 22:51:24 -0400

    changed model to include rnn type - added GRU

--- clean/relation_prediction/model.py
+++ clean/relation_prediction/model.py
@@ -8,16 +8,26 @@ class Encoder(nn.Module):
     def __init__(self, config):
         super(Encoder, self).__init__()
         self.config = config
-        self.rnn = nn.LSTM(input_size=config.d_embed, hidden_size=config.d_hidden,
-                        num_layers=config.n_layers, dropout=config.dropout_prob,
-                        bidirectional=config.birnn)
+        if config.rnn_type.lower() == "gru":
+            self.rnn = nn.GRU(input_size=config.d_embed, hidden_size=config.d_hidden,
+                               num_layers=config.n_layers, dropout=config.dropout_prob,
+                               bidirectional=config.birnn)
+        else:
+            self.rnn = nn.LSTM(input_size=config.d_embed, hidden_size=config.d_hidden,
+                               num_layers=config.n_layers, dropout=config.dropout_prob,
+                               bidirectional=config.birnn)
+
 
     def forward(self, inputs):
         # shape of `inputs` - (sequence length, batch size, dimension of embedding)
         batch_size = inputs.size()[1]
         state_shape = self.config.n_cells, batch_size, self.config.d_hidden
-        h0 = c0 = Variable(inputs.data.new(*state_shape).zero_())
-        outputs, (ht, ct) = self.rnn(inputs, (h0, c0))
+        if self.config.rnn_type.lower() == "gru":
+            h0 = Variable(inputs.data.new(*state_shape).zero_())
+            outputs, ht = self.rnn(inputs, h0)
+        else:
+            h0 = c0 = Variable(inputs.data.new(*state_shape).zero_())
+            outputs, (ht, ct) = self.rnn(inputs, (h0, c0))
         return ht[-1] if not self.config.birnn else ht[-2:].transpose(0, 1).contiguous().view(batch_size, -1)
 
 
@@ -30,7 +40,7 @@ class RelationClassifier(nn.Module):
         self.encoder = Encoder(config)
         self.dropout = nn.Dropout(p=config.dropout_prob)
         self.relu = nn.ReLU()
-        seq_in_size = config.d_hidden # ?? * self.config.n_cells ?? do num layers have any effect?
+        seq_in_size = config.d_hidden
         if self.config.birnn:
             seq_in_size *= 2
 
