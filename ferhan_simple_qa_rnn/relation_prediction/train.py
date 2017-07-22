035859c61aa973f01e673473a27cc795abaa0e74
Salman Mohammed <smohammed1993@gmail.com>
2017-07-22 16:19:05 -0400

    calculate actual index

--- ferhan_simple_qa_rnn/relation_prediction/train.py
+++ ferhan_simple_qa_rnn/relation_prediction/train.py
@@ -196,8 +196,9 @@ for test_batch_idx, test_batch in enumerate(test_iter):
 
      # write to file
      for i, (relations_row, scores_row) in enumerate(zip(top_k_relatons_array, top_k_scores_array)):
-         example = test_batch.dataset.examples[test_batch_idx+i]
-         results_file.write("test-{} %%%% {} %%%% {}\n".format(test_batch_idx+1+i, " ".join(example.question), example.relation))
+         index = (test_batch_idx * args.batch_size) + i
+         example = test_batch.dataset.examples[index]
+         results_file.write("test-{} %%%% {} %%%% {}\n".format(index+1, " ".join(example.question), example.relation))
          for rel, score in zip(relations_row, scores_row):
              results_file.write("{} %%%% {}\n".format(rel, score))
              if (rel == example.relation):
