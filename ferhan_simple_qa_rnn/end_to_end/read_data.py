7d126d61614bfcee56fd5f9b837c266bebd0ca3e
Salman Mohammed <smohammed1993@gmail.com>
2017-08-01 00:08:05 -0400

    read data works
    Former-commit-id: 004d2947156f1c1e6b70da7e5f4087cca88418d4 [formerly 004d2947156f1c1e6b70da7e5f4087cca88418d4 [formerly 4e4d4f4bf17048f2eb7dff4ef8bcf4ddd989edc1]]
    Former-commit-id: f9f1ac486a9eee65bb9a32803a78863d87a9720f
    Former-commit-id: cd00b3e157dff38345d35fbc65a05c576dc61074

--- ferhan_simple_qa_rnn/end_to_end/read_data.py
+++ ferhan_simple_qa_rnn/end_to_end/read_data.py
@@ -4,17 +4,18 @@ hits = 20
 lines = [line.rstrip('\n') for line in open(fname)]
 lines_to_skip = hits + 3
 
-for i in range(lines_to_skip, len(lines), lines_to_skip):
-    if not lines[i-1].startswith("-----------------"):
+for i in range(0, len(lines), lines_to_skip):
+    end_index = i + lines_to_skip - 1
+    if not lines[end_index].startswith("-----------------"):
         print("ERROR!!")
 
-    curr_data = lines[:i-2]
+    curr_data = lines[i:end_index-1]
+    # print(curr_data)
 
     lineid, question, gold_mid = curr_data[0].split(" %% ")
     print("{} - {} - {}".format(lineid, question, gold_mid))
 
     for cand_ent in curr_data[1:]:
-        mid, name, score = cand_ent.split(" %% ")
-        # print("{} - {} - {}".format(mid, name, score))
+       mid, name, score = cand_ent.split(" %% ")
+       # print("{} - {} - {}".format(mid, name, score))
 
-    break
