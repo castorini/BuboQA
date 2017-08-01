3b404b993446e173a700ff488e9b0920de98266a
Salman Mohammed <smohammed1993@gmail.com>
2017-08-01 00:40:08 -0400

    added changes

--- ferhan_simple_qa_rnn/end_to_end/read_data.py
+++ ferhan_simple_qa_rnn/end_to_end/read_data.py
@@ -1,8 +1,12 @@
-fname = "ent-results/out.BruteSearch.2M.h20.txt"
+##----------------------
+## entity linking results
+
+fname = "ent-results/train.BruteSearch.2M.h20.txt"
 
 hits = 20
 lines = [line.rstrip('\n') for line in open(fname)]
 lines_to_skip = hits + 3
+line_ids_ent_found = set()
 
 for i in range(0, len(lines), lines_to_skip):
     end_index = i + lines_to_skip - 1
@@ -13,9 +17,29 @@ for i in range(0, len(lines), lines_to_skip):
     # print(curr_data)
 
     lineid, question, gold_mid = curr_data[0].split(" %% ")
-    print("{} - {} - {}".format(lineid, question, gold_mid))
+    found = "NOT"
+    if lines[end_index - 1].startswith("FOUND"):
+        found = "FOUND"
+        line_ids_ent_found.add(lineid)
 
-    for cand_ent in curr_data[1:]:
-       mid, name, score = cand_ent.split(" %% ")
-       # print("{} - {} - {}".format(mid, name, score))
+    print("{} - {} - {} - {}".format(lineid, found, question, gold_mid))
 
+    # for cand_ent in curr_data[1:]:
+    #    mid, name, score = cand_ent.split(" %% ")
+    #    # print("{} - {} - {}".format(mid, name, score))
+
+
+##----------------------
+## relation prediction results
+
+fname = "rel-results/train-hits-3.txt"
+
+hits = 3
+lines = [line.rstrip('\n') for line in open(fname)]
+lines_to_skip = hits + 2
+line_ids_rel_found = set()
+
+for i in range(0, len(lines), lines_to_skip):
+    end_index = i + lines_to_skip - 1
+    if not lines[end_index].startswith("-----------------"):
+        print("ERROR!!")
