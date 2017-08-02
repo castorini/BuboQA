a8a126363e29ffdb3f60e8871ba12f0508a4e52a
Salman Mohammed <smohammed1993@gmail.com>
2017-08-01 21:15:31 -0400

    changed args according to new paper

--- ferhan_simple_qa_rnn/relation_prediction/hyperparameter_tuning.py
+++ ferhan_simple_qa_rnn/relation_prediction/hyperparameter_tuning.py
@@ -1,13 +1,13 @@
 from random import randint, uniform
 from subprocess import call
 
-epochs = 50
-count = 20
+epochs = 20
+count = 50
 for id in range(count):
-    learning_rate = 10 ** uniform(-5, -4)
-    d_hidden = randint(550, 600)
-    n_layers = randint(4, 5)
-    dropout = uniform(0.5, 0.6)
+    learning_rate = 10 ** uniform(-6, -1)
+    d_hidden = randint(200, 600)
+    n_layers = 2
+    dropout = uniform(0.1, 0.15)
     clip = uniform(0.6, 0.7)
 
     command = "python train.py --dev_every 500 --log_every 250 --batch_size 32 " \
