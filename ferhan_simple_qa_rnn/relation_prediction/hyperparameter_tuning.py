3d3c49685aa645afc36b04affc34fd5655702968
Salman Mohammed <smohammed1993@gmail.com>
2017-08-02 17:07:59 -0400

    tuning results and default args changed

--- ferhan_simple_qa_rnn/relation_prediction/hyperparameter_tuning.py
+++ ferhan_simple_qa_rnn/relation_prediction/hyperparameter_tuning.py
@@ -1,14 +1,14 @@
 from random import randint, uniform
 from subprocess import call
 
-epochs = 20
-count = 40
+epochs = 30
+count = 10
 for id in range(count):
-    learning_rate = 10 ** uniform(-6, -2)
-    d_hidden = randint(80, 400)
-    n_layers = randint(2, 3)
-    dropout = uniform(0.1, 0.5)
-    clip = uniform(0.5, 0.7)
+    learning_rate = 1e-4
+    d_hidden = randint(100, 300)
+    n_layers = 2
+    dropout = 0.3
+    clip = 0.6
 
     command = "python train.py --dev_every 2200 --log_every 2200 --save_every 5000 --batch_size 32 " \
                 "--epochs {} --lr {} --d_hidden {} --n_layers {} --dropout_prob {} --clip_gradient {} >> " \
