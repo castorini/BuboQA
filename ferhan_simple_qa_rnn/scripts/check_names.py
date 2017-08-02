17c436f0c30f36bba2dbfd30b74c816cfdfccaff
Salman Mohammed <smohammed1993@gmail.com>
2017-08-02 17:03:41 -0400

    add imp scripts and gold entity detection results

---
+++ ferhan_simple_qa_rnn/scripts/check_names.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+
+import sys
+import argparse
+
+from util import www2fb, clean_uri
+
+def get_names_for_entities(namespath):
+    print("getting names map...")
+    names = {}
+    with open(namespath, 'r') as f:
+        for i, line in enumerate(f):
+            if i % 1000000 == 0:
+                print("line: {}".format(i))
+
+            items = line.strip().split("\t")
+            if len(items) != 4:
+                print("ERROR: line - {}".format(line))
+            entity = clean_uri(items[0])
+            type = clean_uri(items[1])
+            literal = clean_uri(items[2])
+            if entity not in names.keys():
+                names[entity] = [literal]
+            else:
+                names[entity].append(literal)
+    return names
+
+def check_names(datapath, namespath, outpath):
+    names_map = get_names_for_entities(namespath)
+    notfound = 0
+    total = 0
+    outfile = open(outpath, 'w')
+    with open(datapath, 'r') as f:
+        for i, line in enumerate(f):
+            total += 1
+            if i % 1000000 == 0:
+                print("line: {}".format(i))
+
+            items = line.strip().split("\t")
+            if len(items) != 5:
+                print("ERROR: line - {}".format(line))
+                sys.exit(0)
+
+            lineid = items[0]
+            subject = www2fb(items[1])
+            predicate = www2fb(items[2])
+            object = www2fb(items[3])
+            question = items[4]
+
+            line_to_print = "{}\t{}\t{}\t{}\t{}".format(lineid, subject, predicate, object, question);
+
+            if subject not in names_map.keys():
+                print("WARNING: name not found in map. line - {}".format(line))
+                notfound += 1
+                outfile.write(line_to_print + "\n")
+
+            print(line_to_print)
+
+    print("notfound: {}".format(notfound))
+    print("found: {}".format(total-notfound))
+    outfile.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Check which examples have entities that do not have name mappings')
+    parser.add_argument('-d', '--data', dest='data', action='store', required = True,
+                        help='path to the NUMBERED dataset all-data.txt file')
+    parser.add_argument('-n', '--names', dest='names', action='store', required=True,
+                        help='path to the names file (from CFO)')
+    parser.add_argument('-o', '--output', dest='output', action='store', required=True,
+                        help='output file for list of examples that do not exist')
+
+    args = parser.parse_args()
+    print("Dataset: {}".format(args.data))
+    print("Names: {}".format(args.names))
+    print("Output: {}".format(args.output))
+
+    check_names(args.data, args.names, args.output)
+    print("Checked the names.")
---
+++ ferhan_simple_qa_rnn/scripts/create_inverted_index_entity.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+
+import sys
+import argparse
+import pickle
+
+from nltk.tokenize.moses import MosesTokenizer
+from util import www2fb, clean_uri
+
+
+def get_all_ngrams(tokens):
+    all_ngrams = set()
+    max_n = min(len(tokens), 3)
+    for n in range(1, max_n+1):
+        ngrams = find_ngrams(tokens, n)
+        all_ngrams = all_ngrams | ngrams
+    return all_ngrams
+
+def find_ngrams(input_list, n):
+    ngrams = zip(*[input_list[i:] for i in range(n)])
+    return set(ngrams)
+
+def create_inverted_index_entity(namespath, outpath):
+    print("creating the index map...")
+    index = {}
+    tokenizer = MosesTokenizer()
+    size = 0
+    with open(namespath, 'r') as f:
+        for i, line in enumerate(f):
+            if i % 1000000 == 0:
+                print("line: {}".format(i))
+
+            items = line.strip().split("\t")
+            if len(items) != 4:
+                print("ERROR: line - {}".format(line))
+
+            entity_mid = clean_uri(items[0])
+            entity_type = clean_uri(items[1])
+            entity_name = clean_uri(items[2])
+
+            try:
+                name_tokens = tokenizer.tokenize(entity_name)
+                name_ngrams = get_all_ngrams(name_tokens)
+            except:
+                name_ngrams = set()
+                name_ngrams.add( (entity_name,) )
+                name_ngrams.add( (entity_name.replace(".", ""),) )
+                # print(entity_name)
+
+            # print(line.strip())
+            for ngram_tuple in name_ngrams:
+                size += 1
+                ngram = " ".join(ngram_tuple)
+                # print(ngram)
+                if ngram in index.keys():
+                    index[ngram].add(entity_mid)
+                else:
+                    index[ngram] = set([entity_mid])
+
+
+    print("num keys: {}".format(len(index)))
+    print("total key-value pairs: {}".format(size))
+
+    print("dumping to pickle...")
+    with open(outpath, 'wb') as f:
+        pickle.dump(index, f)
+
+    print("DONE")
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Create inverted index for entity')
+    parser.add_argument('-n', '--names', dest='names', action='store', required=True,
+                        help='path to the names file (from CFO)')
+    parser.add_argument('-p', '--pickle', dest='pickle', action='store', required=True,
+                        help='output file for the index pickle to be dumped')
+
+    args = parser.parse_args()
+    print("Names: {}".format(args.names))
+    print("Pickle output: {}".format(args.pickle))
+
+    create_inverted_index_entity(args.names, args.pickle)
+    print("Created the entity index.")
