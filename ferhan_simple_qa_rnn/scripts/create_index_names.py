3ec7d02b1ca684831518b68ab7e538aa093f34e5
Salman Mohammed <smohammed1993@gmail.com>
2017-08-03 02:00:32 -0400

    entity linking
    Former-commit-id: 75a87c640102ab0f518a70674d7e446891e2e5f4 [formerly d660528b011b4ac7ba284e56084a0e3d4976a421 [formerly a8c53854f7f1f42a9ae01d60ec9f04f43c22d339]]
    Former-commit-id: f6143450254ecaf953afa3b13a89a6ff53fc1fc1
    Former-commit-id: ae910dbb4c2ee69cb9c5550af3de824c90bdd621

---
+++ ferhan_simple_qa_rnn/scripts/create_index_names.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import sys
+import argparse
+import pickle
+
+from util import www2fb, clean_uri
+
+def get_names_map(namespath):
+    print("getting names map...")
+    names = {}
+    with open(namespath, 'r') as f:
+        for i, line in enumerate(f):
+            if i % 1000000 == 0:
+                print("line: {}".format(i))
+
+            items = line.strip().split("\t")
+            if len(items) != 4:
+                print("ERROR: line - {}".format(line))
+            entity = clean_uri(items[0])
+            type = clean_uri(items[1])
+            literal = clean_uri(items[2])
+            if entity not in names.keys():
+                names[entity] = [literal]
+            else:
+                names[entity].append(literal)
+    return names
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Created the names index")')
+    parser.add_argument('-n', '--names', dest='names', action='store', required=True,
+                        help='path to the names file (from CFO)')
+    parser.add_argument('-p', '--pickle', dest='pickle', action='store', required=True,
+                        help='output file path for the pickle of names index')
+
+    args = parser.parse_args()
+    print("Names file path: {}".format(args.names))
+    print("Pickle output path: {}".format(args.pickle))
+
+    index_names = get_names_map(args.names)
+
+    with open(args.pickle, 'wb') as f:
+        pickle.dump(index_names, f)
+
+    print("Created the names index.")
