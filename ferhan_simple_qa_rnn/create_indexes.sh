880bfef15b6d5c09d9dc4379cf20a8424d8710cd
Salman Mohammed <smohammed1993@gmail.com>
2017-08-11 13:29:00 -0400

    changed script

--- ferhan_simple_qa_rnn/create_indexes.sh
+++ ferhan_simple_qa_rnn/create_indexes.sh
@@ -4,7 +4,7 @@
 mkdir indexes
 
 echo -e "\n\nCreate the names map index..."
-python scripts/create_index_names.py -n data/FB5M.name.txt -p indexes/names.pkl
+python scripts/create_index_names.py -d data/SimpleQuestions_v2_modified/all.txt -n data/FB5M.name.txt -p indexes/names.pkl
 
 echo -e "\n\nCreate the reachability index for 2M-freebase-subset..."
 python scripts/create_index_reachability.py -s data/SimpleQuestions_v2/freebase-subsets/freebase-FB2M.txt -p indexes/reachability_2M.pkl
