183d2af934ead581ae7d63c66670a95b08c84213
Salman Mohammed <smohammed1993@gmail.com>
2017-08-11 15:59:53 -0400

    changed echo style

--- ferhan_simple_qa_rnn/create_indexes.sh
+++ ferhan_simple_qa_rnn/create_indexes.sh
@@ -3,22 +3,22 @@
 # creating the indexes
 mkdir indexes
 
-echo -e "\n\nCreate the names map index for 2M-freebase-subset..."
+echo "\n\nCreate the names map index for 2M-freebase-subset..."
 python scripts/create_index_names.py -d data/SimpleQuestions_v2_modified/all.txt -n data/names.trimmed.2M.txt -p indexes/names_2M.pkl
 
-# echo -e "\n\nCreate the names map index for 5M-freebase-subset..."
+# echo "\n\nCreate the names map index for 5M-freebase-subset..."
 # python scripts/create_index_names.py -d data/SimpleQuestions_v2_modified/all.txt -n data/names.trimmed.5M.txt -p indexes/names_5M.pkl
 
-echo -e "\n\nCreate the reachability index for 2M-freebase-subset..."
+echo "\n\nCreate the reachability index for 2M-freebase-subset..."
 python scripts/create_index_reachability.py -s data/SimpleQuestions_v2/freebase-subsets/freebase-FB2M.txt -p indexes/reachability_2M.pkl
 
-# echo -e "\n\nCreate the reachability index for 5M-freebase-subset..."
+# echo "\n\nCreate the reachability index for 5M-freebase-subset..."
 # python scripts/create_index_reachability.py -s data/SimpleQuestions_v2/freebase-subsets/freebase-FB5M.txt -p indexes/reachability_5M.pkl
 
-echo -e "\n\nCreate the inverted index for entity names for 2M-freebase-subset..."
+echo "\n\nCreate the inverted index for entity names for 2M-freebase-subset..."
 python scripts/create_inverted_index_entity.py -n data/names.trimmed.2M.txt -p indexes/entity_2M.pkl
 
-# echo -e "\n\nCreate the inverted index for entity names for 5M-freebase-subset..."
+# echo "\n\nCreate the inverted index for entity names for 5M-freebase-subset..."
 # python scripts/create_inverted_index_entity.py -n data/names.trimmed.5M.txt -p indexes/entity_5M.pkl
 
-echo -e "\n\nDONE!"
+echo "\n\nDONE!"
