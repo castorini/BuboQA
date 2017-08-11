61494413457564a6aeeade47461ba1c216ffde07
Salman Mohammed <smohammed1993@gmail.com>
2017-08-11 12:36:16 -0400

    changed fetch script
    Former-commit-id: b5344573671dcb9284b7cdef205640ce2b4078e0 [formerly 2202949910ec6bf0a54b2eccc0abc833fb02b4f8 [formerly 060d3a6087a81b47f64bca0609e5a2c30a2f59b4]]
    Former-commit-id: 388f1878e416f8543e5c96d28f58ffe74baf27cf
    Former-commit-id: 78c7cac856f0bb3a5db932223deec9f8f652cebf

--- ferhan_simple_qa_rnn/fetch_dataset.sh
+++ ferhan_simple_qa_rnn/fetch_dataset.sh
@@ -3,6 +3,10 @@
 # download dataset and put it in data directory
 mkdir data
 pushd data
+echo "Downloading SimpleQuestions dataset..."
 wget https://www.dropbox.com/s/tohrsllcfy7rch4/SimpleQuestions_v2.tgz
+echo "Unzipping SimpleQuestions dataset..."
 tar -xvzf SimpleQuestions_v2.tgz
+echo "Downloading the names file..."
+wget https://www.dropbox.com/s/yqbesl07hsw297w/FB5M.name.txt
 popd
