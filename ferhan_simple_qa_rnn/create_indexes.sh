9f1537028da8ad6758f6ed0345018c414b07403a
Salman Mohammed <smohammed1993@gmail.com>
2017-08-11 13:00:00 -0400

    inverted index entity

--- ferhan_simple_qa_rnn/create_indexes.sh
+++ ferhan_simple_qa_rnn/create_indexes.sh
@@ -13,6 +13,6 @@ echo -e "\n\nCreate the reachability index for 5M-freebase-subset..."
 python scripts/create_index_reachability.py -s data/SimpleQuestions_v2/freebase-subsets/freebase-FB5M.txt -p indexes/reachability_5M.pkl
 
 echo -e "\n\nCreate the inverted index for entity names..."
-python scripts/create_index_names.py -n data/FB5M.name.txt -p indexes/entity.pkl
+python scripts/create_inverted_index_entity.py -n data/FB5M.name.txt -p indexes/entity.pkl
 
 echo -e "\n\nDONE!"
