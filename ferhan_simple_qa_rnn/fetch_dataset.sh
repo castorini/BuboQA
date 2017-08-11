95328f845872fe1e43011b5eb67844e2f4672e2c
Salman Mohammed <smohammed1993@gmail.com>
2017-08-11 12:59:20 -0400

    pretty print

--- ferhan_simple_qa_rnn/fetch_dataset.sh
+++ ferhan_simple_qa_rnn/fetch_dataset.sh
@@ -3,12 +3,19 @@
 # download dataset and put it in data directory
 mkdir data
 pushd data
-echo "Downloading SimpleQuestions dataset..."
+
+echo -e "\n\nDownloading SimpleQuestions dataset..."
 wget https://www.dropbox.com/s/tohrsllcfy7rch4/SimpleQuestions_v2.tgz
-echo "Unzipping SimpleQuestions dataset..."
+
+echo -e "\n\nUnzipping SimpleQuestions dataset..."
 tar -xvzf SimpleQuestions_v2.tgz
-echo "Downloading the names file..."
+
+echo -e "\n\nDownloading the names file..."
 wget https://www.dropbox.com/s/yqbesl07hsw297w/FB5M.name.txt
+
 popd
-echo "Create modified dataset..."
+
+echo -e "\n\nCreate modified dataset..."
 python scripts/modify_dataset.py -d data/SimpleQuestions_v2 -o data/SimpleQuestions_v2_modified
+
+echo -e "\n\nDONE!"
