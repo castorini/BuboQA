2a38216105807376d4e1db86204b4aa12cf40d3a
Salman Mohammed <smohammed1993@gmail.com>
2017-08-11 15:59:53 -0400

    changed echo style
    Former-commit-id: 24a9198150cc7fb243929244107b5f04ddcc87b2 [formerly a12465ff004f0696409fbb749073aab27bda7e06 [formerly 555e67522c77a1631c4b9d6c79e719838dce0371]]
    Former-commit-id: 63c578135551e457fd588a2ec4e6f8135e19cb64
    Former-commit-id: 75b25db2fa451c1507eeccc67100d8ec146ac3c4

--- ferhan_simple_qa_rnn/fetch_dataset.sh
+++ ferhan_simple_qa_rnn/fetch_dataset.sh
@@ -4,24 +4,24 @@
 mkdir data
 pushd data
 
-echo -e "\n\nDownloading SimpleQuestions dataset..."
+echo "\n\nDownloading SimpleQuestions dataset..."
 wget https://www.dropbox.com/s/tohrsllcfy7rch4/SimpleQuestions_v2.tgz
 
-echo -e "\n\nUnzipping SimpleQuestions dataset..."
+echo "\n\nUnzipping SimpleQuestions dataset..."
 tar -xvzf SimpleQuestions_v2.tgz
 
-echo -e "\n\nDownloading the names file..."
+echo "\n\nDownloading the names file..."
 wget https://www.dropbox.com/s/yqbesl07hsw297w/FB5M.name.txt
 
 popd
 
-echo -e "\n\nTrimming the names file for subset 2M..."
+echo "\n\nTrimming the names file for subset 2M..."
 python scripts/trim_names.py -s data/SimpleQuestions_v2/freebase-subsets/freebase-FB2M.txt -n data/FB5M.name.txt -o data/names.trimmed.2M.txt
 
-echo -e "\n\nTrimming the names file for subset 5M..."
+echo "\n\nTrimming the names file for subset 5M..."
 python scripts/trim_names.py -s data/SimpleQuestions_v2/freebase-subsets/freebase-FB5M.txt -n data/FB5M.name.txt -o data/names.trimmed.5M.txt
 
-echo -e "\n\nCreate modified dataset..."
+echo "\n\nCreate modified dataset..."
 python scripts/modify_dataset.py -d data/SimpleQuestions_v2 -o data/SimpleQuestions_v2_modified
 
-echo -e "\n\nDONE!"
+echo "\n\nDONE!"
