341525b4e5d979c0deec2a3a567fc5b7d8efe6bc
Salman Mohammed <smohammed1993@gmail.com>
2017-08-11 17:01:51 -0400

    evaluate linking script

--- ferhan_simple_qa_rnn/entity_linking/evaluate.py
+++ ferhan_simple_qa_rnn/entity_linking/evaluate.py
@@ -13,12 +13,12 @@ def get_predictions(datapath):
         for line in f:
             items = line.strip().split(" %%%% ")
             lineid = items[0].strip()
-            ent_mid = items[1].strip()
+            mid = items[1].strip()
             rel = items[2].strip()
-            # print("{}   -   {}".format(lineid, rel))
+            # print("{}  -  {}  -  {}".format(lineid, mid, rel))
             lineids.append(lineid)
-            id2rel[lineid] = rel
-    return lineids, id2rel
+            id2pred[lineid] = (mid, rel)
+    return lineids, id2pred
 
 def get_gold_labels(datapath):
     lineids = []
@@ -36,66 +36,57 @@ def get_gold_labels(datapath):
             lineid = items[0]
             subject = www2fb(items[1])
             predicate = www2fb(items[2])
+            # print("{}  -  {}  -  {}".format(lineid, subject, predicate))
             lineids.append(lineid)
             id2label[lineid] = (subject, predicate)
     return lineids, id2label
 
 
 def evaluate(goldpath, predpath):
-    allpath = os.path.join(outdir, "all.txt")
-    outallfile = open(allpath, 'w')
-    names_map = names_map = get_index(index_namespath)
-    # files = [("annotated_fb_data_train", "train"), ("annotated_fb_data_valid", "val"), ("annotated_fb_data_test", "test")]
-    files = [("train", "train"), ("val", "val"), ("test", "test")]
-    for f_tuple in files:
-        f = f_tuple[0]
-        fname = f_tuple[1]
-        in_fpath = os.path.join(datadir, f + ".txt")
-        out_fpath = os.path.join(outdir, fname + ".txt")
-        notfound = 0
-        total = 0
-        outfile = open(out_fpath, 'w')
-        print("processing dataset: {}".format(fname))
-        with open(in_fpath, 'r') as f:
-            for i, line in enumerate(f):
-                total += 1
-                if i % 1000000 == 0:
-                    print("line: {}".format(i))
+    gold_lineids, gold_id2labels = get_gold_labels(goldpath)
+    pred_lineids, pred_id2preds = get_predictions(predpath)
 
-                items = line.strip().split("\t")
-                if len(items) != 5:
-                    print("ERROR: line - {}".format(line))
-                    sys.exit(1)
+    not_found = 0
+    found = 0
 
-                lineid = items[0]
-                subject = www2fb(items[1])
-                predicate = www2fb(items[2])
-                object = www2fb(items[3])
-                question = items[4]
-                if subject not in names_map.keys():
-                    print("WARNING: name not found in map. line - {}".format(line))
-                    notfound += 1
-                    continue
+    correct = 0
+    mid_wrong = 0
+    rel_wrong = 0
+    both_wrong = 0
 
-                name = names_map[subject]
-                if name.strip() == "":
-                    print("WARNING: name stripped empty. line - {}".format(line))
-                    notfound += 1
-                    continue
+    for lineid in gold_lineids:
+        if lineid in pred_lineids:
+            found += 1
+            pred_mid, pred_rel = pred_id2preds[lineid]
+            gold_mid, gold_rel = gold_id2labels[lineid]
+            if pred_mid != gold_mid and pred_rel != gold_rel:
+                both_wrong += 1
+            elif pred_mid != gold_mid:
+                mid_wrong += 1
+            elif pred_rel != gold_rel:
+                rel_wrong += 1
+            else:
+                correct += 1
+        else:
+            not_found += 1
+            continue
 
-                line_to_print = "{} %%%% {} %%%% {}".format(lineid, subject, name)
-                # print(line_to_print)
-                outfile.write(line_to_print + "\n")
-                outallfile.write(line_to_print + "\n")
+    total = found + not_found
+    all_wrong = both_wrong + mid_wrong + rel_wrong
+    accuracy = 100.0 * (correct / total)
 
-        print("done with dataset: {}".format(fname))
-        print("notfound: {}".format(notfound))
-        print("found: {}".format(total-notfound))
-        print("-" * 60)
-        outfile.close()
+    print("total: {}".format(total))
+    print("found: {}".format(found))
+    print("not found: {}".format(not_found))
+    print("both wrong: {}".format(both_wrong))
+    print("only mid wrong: {}".format(mid_wrong))
+    print("only rel wrong: {}".format(rel_wrong))
 
-    outallfile.close()
-    print("DONE!")
+    print("all wrong: {}".format(all_wrong))
+    print("all wrong (including not found): {}".format(all_wrong + not_found))
+    print("correct: {}".format(correct))
+    print("Accuracy: {}%".format(accuracy))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Get the gold query text after entity detection')
