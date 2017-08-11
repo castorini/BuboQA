057fc8fa07b426d1297c8d52c24960ced6a721c9
Salman Mohammed <smohammed1993@gmail.com>
2017-08-11 17:02:44 -0400

    paste ww2fb from util

--- ferhan_simple_qa_rnn/entity_linking/evaluate.py
+++ ferhan_simple_qa_rnn/entity_linking/evaluate.py
@@ -4,7 +4,18 @@ import os
 import sys
 import argparse
 
-from util import www2fb, clean_uri
+
+def www2fb(in_str):
+    if in_str.startswith("www.freebase.com"):
+        return 'fb:%s' % (in_str.split('www.freebase.com/')[-1].replace('/', '.'))
+    return in_str
+
+def clean_uri(uri):
+    if uri.startswith("<") and uri.endswith(">"):
+        return clean_uri(uri[1:-1])
+    elif uri.startswith("\"") and uri.endswith("\""):
+        return clean_uri(uri[1:-1])
+    return uri
 
 def get_predictions(datapath):
     lineids = []
