6ca47795b46753a6f02778fda4c44b785453f5d8
Salman Mohammed <smohammed1993@gmail.com>
2017-08-11 15:45:54 -0400

    trim names from fb subset
    Former-commit-id: 6a87ac145bd8c0fd8cf1f4bf65f6c662fcbace78 [formerly a7b85340b5ea9095731ff719180b3ee34038522a [formerly cda622494584a0d114ee16351e6b04d9063b15e2]]
    Former-commit-id: d12f07f6c0a6faf0aa0ec21bd03a2bc9fccd9e98
    Former-commit-id: ac47f723a7d60b915825240fc5b995177d1120bb

--- ferhan_simple_qa_rnn/scripts/trim_names.py
+++ ferhan_simple_qa_rnn/scripts/trim_names.py
@@ -7,26 +7,26 @@ import pickle
 from util import www2fb, clean_uri
 
 
-def get_all_entity_mids(datapath):
+def get_all_entity_mids(fbpath):
+    print("getting all entity MIDs from Freebase subset...")
     mids = set()
-    with open(datapath, 'r') as f:
+    with open(fbpath, 'r') as f:
         for i, line in enumerate(f):
             if i % 1000000 == 0:
                 print("line: {}".format(i))
 
             items = line.strip().split("\t")
-            if len(items) != 5:
+            if len(items) != 3:
                 print("ERROR: line - {}".format(line))
-                sys.exit(0)
 
-            lineid = items[0]
-            subject = www2fb(items[1])
+            subject = www2fb(items[0])
             mids.add(subject)
+
     return mids
 
 
-def trim_names(datapath, namespath, outpath):
-    mids_to_check = get_all_entity_mids(datapath=datapath)
+def trim_names(fbsubsetpath, namespath, outpath):
+    mids_to_check = get_all_entity_mids(fbsubsetpath)
     outfile = open(outpath, 'w')
     with open(namespath, 'r') as f:
         for i, line in enumerate(f):
@@ -46,17 +46,17 @@ def trim_names(datapath, namespath, outpath):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Trim the names file')
-    parser.add_argument('-d', '--data', dest='data', action='store', required=True,
-                        help='path to the NUMBERED dataset all.txt file')
+    parser.add_argument('-s', '--fbsubset', dest='fbsubset', action='store', required=True,
+                        help='path to freebase subset file')
     parser.add_argument('-n', '--names', dest='names', action='store', required=True,
                         help='path to the names file (from CFO)')
     parser.add_argument('-o', '--output', dest='output', action='store', required=True,
                         help='output file path for trimmed names file')
 
     args = parser.parse_args()
-    print("Dataset: {}".format(args.data))
+    print("Freebase subset: {}".format(args.fbsubset))
     print("Names file from CFO: {}".format(args.names))
     print("Output trimmed names file: {}".format(args.output))
 
-    trim_names(args.data, args.names, args.output)
+    trim_names(args.fbsubset, args.names, args.output)
     print("Trimmed the names file.")
