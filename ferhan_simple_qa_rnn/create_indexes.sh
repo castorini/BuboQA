2a9dcb4cb7c10d27d4414bc4ec345eeaae07b9a8
Salman Mohammed <smohammed1993@gmail.com>
2017-08-11 12:59:20 -0400

    pretty print
    Former-commit-id: 710a371130679a1a491383547235a0f136bd9585 [formerly d711412d68ee41f00d22440e795781223972fecd [formerly dcd4d7ca54e0fa1051639dc3146d9057d93e7585]]
    Former-commit-id: 04a0ea75a582ed66b909de7acc9c6bcb8a401994
    Former-commit-id: 9b16f2d6def446e00a4bcc8ad458bfe25720b4b5

--- ferhan_simple_qa_rnn/create_indexes.sh
+++ ferhan_simple_qa_rnn/create_indexes.sh
@@ -3,16 +3,16 @@
 # creating the indexes
 mkdir indexes
 
-echo "Create the names map index..."
+echo -e "\n\nCreate the names map index..."
 python scripts/create_index_names.py -n data/FB5M.name.txt -p indexes/names.pkl
 
-echo "Create the reachability index for 2M-freebase-subset..."
+echo -e "\n\nCreate the reachability index for 2M-freebase-subset..."
 python scripts/create_index_reachability.py -s data/SimpleQuestions_v2/freebase-subsets/freebase-FB2M.txt -p indexes/reachability_2M.pkl
 
-echo "Create the reachability index for 5M-freebase-subset..."
+echo -e "\n\nCreate the reachability index for 5M-freebase-subset..."
 python scripts/create_index_reachability.py -s data/SimpleQuestions_v2/freebase-subsets/freebase-FB5M.txt -p indexes/reachability_5M.pkl
 
-echo "Create the inverted index for entity names..."
+echo -e "\n\nCreate the inverted index for entity names..."
 python scripts/create_index_names.py -n data/FB5M.name.txt -p indexes/entity.pkl
 
-echo "DONE!"
+echo -e "\n\nDONE!"
