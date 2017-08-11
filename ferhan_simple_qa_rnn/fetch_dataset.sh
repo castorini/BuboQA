e7f77b47921ffd22485c21bf597ea708e842563c
Salman Mohammed <smohammed1993@gmail.com>
2017-08-11 16:00:51 -0400

    pretty print

--- ferhan_simple_qa_rnn/fetch_dataset.sh
+++ ferhan_simple_qa_rnn/fetch_dataset.sh
@@ -4,24 +4,24 @@
 mkdir data
 pushd data
 
-echo "\n\nDownloading SimpleQuestions dataset..."
+echo "Downloading SimpleQuestions dataset...\n"
 wget https://www.dropbox.com/s/tohrsllcfy7rch4/SimpleQuestions_v2.tgz
 
-echo "\n\nUnzipping SimpleQuestions dataset..."
+echo "\n\nUnzipping SimpleQuestions dataset...\n"
 tar -xvzf SimpleQuestions_v2.tgz
 
-echo "\n\nDownloading the names file..."
+echo "\n\nDownloading the names file...\n"
 wget https://www.dropbox.com/s/yqbesl07hsw297w/FB5M.name.txt
 
 popd
 
-echo "\n\nTrimming the names file for subset 2M..."
+echo "\n\nTrimming the names file for subset 2M...\n"
 python scripts/trim_names.py -s data/SimpleQuestions_v2/freebase-subsets/freebase-FB2M.txt -n data/FB5M.name.txt -o data/names.trimmed.2M.txt
 
-echo "\n\nTrimming the names file for subset 5M..."
+echo "\n\nTrimming the names file for subset 5M...\n"
 python scripts/trim_names.py -s data/SimpleQuestions_v2/freebase-subsets/freebase-FB5M.txt -n data/FB5M.name.txt -o data/names.trimmed.5M.txt
 
-echo "\n\nCreate modified dataset..."
+echo "\n\nCreate modified dataset...\n"
 python scripts/modify_dataset.py -d data/SimpleQuestions_v2 -o data/SimpleQuestions_v2_modified
 
 echo "\n\nDONE!"
