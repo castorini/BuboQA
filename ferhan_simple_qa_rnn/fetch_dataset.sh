c0b73b46ef257dd6669cb9da07bd9bd24d213340
Salman Mohammed <smohammed1993@gmail.com>
2017-08-11 15:49:17 -0400

    changed script to trim names
    Former-commit-id: 781269984c4c171af810082476c95b8d94a3cdbd [formerly 37b3437b1ed09ba71f4271b143f4ebc4828bec6b [formerly 394307b5e2a6aa904f79cc1ec6c8dbd85a383e6a]]
    Former-commit-id: 68eae4a09dbd4675af390a4d55e55163fb39812f
    Former-commit-id: 16b113bae281180f28e8b6787893164e03e44f13

--- ferhan_simple_qa_rnn/fetch_dataset.sh
+++ ferhan_simple_qa_rnn/fetch_dataset.sh
@@ -15,6 +15,12 @@ wget https://www.dropbox.com/s/yqbesl07hsw297w/FB5M.name.txt
 
 popd
 
+echo -e "\n\nTrimming the names file for subset 2M..."
+python scripts/trim_names.py -s data/SimpleQuestions_v2/freebase-subsets/freebase-FB2M.txt -n data/FB5M.name.txt -o data/names.trimmed.2M.txt
+
+echo -e "\n\nTrimming the names file for subset 5M..."
+python scripts/trim_names.py -s data/SimpleQuestions_v2/freebase-subsets/freebase-FB5M.txt -n data/FB5M.name.txt -o data/names.trimmed.5M.txt
+
 echo -e "\n\nCreate modified dataset..."
 python scripts/modify_dataset.py -d data/SimpleQuestions_v2 -o data/SimpleQuestions_v2_modified
 
