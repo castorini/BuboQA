ab9c0ebf1de1e1bce342b4079760111c8066a955
Salman Mohammed <smohammed1993@gmail.com>
2017-08-12 10:11:29 -0400

    improved retrieval printing

--- ferhan_simple_qa_rnn/relation_prediction/top_k_retrieval.py
+++ ferhan_simple_qa_rnn/relation_prediction/top_k_retrieval.py
@@ -82,8 +82,9 @@ def write_top_k_results(dataset_iter=train_iter, dataset=train, data_name="train
                 #     results_file.write("NOT found\n")
                 # results_file.write("-" * 60 + "\n")
 
+    print("no. retrieved: {} out of {}".format(n_retrieved, len(dataset)))
     retrieval_rate = 100. * n_retrieved / len(dataset)
-    print("retrieval rate (hits = {}): {:8.6f}%".format(args.hits, retrieval_rate))
+    print("{} retrieval rate (hits = {}): {:8.6f}%".format(data_name, args.hits, retrieval_rate))
     print("no. correct: {} out of {}".format(n_correct, len(dataset)))
     accuracy = 100. * n_correct / len(dataset)
     print("{} accuracy: {:8.6f}%".format(data_name, accuracy))
