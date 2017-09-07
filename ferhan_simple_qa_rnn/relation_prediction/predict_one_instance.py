6f143f0facc2f56a12afd042bdb00adcf9016c0a
Salman Mohammed <smohammed1993@gmail.com>
2017-09-07 12:57:10 -0400

    relation prediction for one instance
    Former-commit-id: ffcb709460def1a4d756599a03135afcb10e53bd [formerly 82da76b8eb24a6184dfc3201a1cdde42e2de67f2 [formerly 5f6419e111eeb06c2342e6bb4288423fcf9fb1b5]]
    Former-commit-id: 7069f01b1e3d362bfb2cc75ebc72ebea898672c0
    Former-commit-id: 0aa77f66c4e421e2bffbb7ff4d9d83f77ae81758

---
+++ ferhan_simple_qa_rnn/relation_prediction/predict_one_instance.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import numpy as np
+import torch
+
+from torchtext import data
+from nltk.tokenize.treebank import TreebankWordTokenizer
+from args import get_args
+from simple_qa_relation import SimpleQaRelationDataset
+
+# get the configuration arguments and set machine - GPU/CPU
+args = get_args()
+# set random seeds for reproducibility
+torch.manual_seed(args.seed)
+if not args.cuda:
+    args.gpu = -1
+if torch.cuda.is_available() and not args.cuda:
+    print("WARNING: You have CUDA but not using it.")
+if torch.cuda.is_available() and args.cuda:
+    torch.cuda.set_device(args.gpu)
+    torch.cuda.manual_seed(args.seed)
+
+if not args.trained_model:
+    print("ERROR: You need to provide a option 'trained_model' path to load the model.")
+    sys.exit(1)
+
+# ---- get the Field, Dataset, Iterator for train/dev/test sets -----
+tokenizer = TreebankWordTokenizer()
+def tokenize_text():
+    return lambda text: tokenizer.tokenize(text)
+
+questions = data.Field(lower=True, tokenize=tokenize_text())
+relations = data.Field(sequential=False)
+
+train, dev, test = SimpleQaRelationDataset.splits(questions, relations)
+train_iter, dev_iter, test_iter = SimpleQaRelationDataset.iters(args, questions, relations, train, dev, test, shuffleTrain=False)
+
+# load the model
+model = torch.load(args.trained_model, map_location=lambda storage,location: storage.cuda(args.gpu))
+
+index2rel = np.array(relations.vocab.itos)
+
+input_sent = "where was sasha vujacic born?"
+
+class ins(object):
+    def __init__(self, question):
+      self.question = question
+
+
+def get_relation(input_sent):
+    sent = tokenizer.tokenize(input_sent.lower())
+    example = ins(questions.numericalize(questions.pad([sent]), device=args.gpu, train=False))
+    model.eval()
+    scores = model(example)
+    # get the predicted relations
+    top_scores, top_indices = torch.max(scores, dim=1)  # shape: (batch_size, 1)
+    top_index = top_indices.cpu().data.numpy()[0][0]
+    predicted_relation = index2rel[top_index]
+    return predicted_relation
+
+print("question: {}".format(input_sent))
+print("predicted relation: {}".format(get_relation(input_sent)))
