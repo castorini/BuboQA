9d20a39e3c5e43cc1d2456b22377179fd60c078c
Peng Shi <spone.2014@gmail.com>
2017-11-26 18:40:40 -0500

    Update the arguments for relation prediction (#45)
    * organized relation prediction into nn(LSTM GRU CNN), add docs
    
    * convert torchtext to 0.2.0, interface updates
    
    * entity detection crf model
    
    * fix args parser
    
    * Entity Detection NN models, fix fetch_data script, fix lr_w2v_rel id mismatch
    
    * auto config for data path in sq_dataset.py
    
    * rename retrieval as top_retrieval
    
    * move temp file from /tmp to local dir
    
    * update documentation
    
    * update documents
    
    * config data_dir in relation prediction

--- relation_prediction/nn/sq_relation_dataset.py
+++ relation_prediction/nn/sq_relation_dataset.py
@@ -2,9 +2,8 @@ from torchtext import data
 
 class SQdataset(data.TabularDataset):
     @classmethod
-    def splits(cls, text_field, label_field,
+    def splits(cls, text_field, label_field, path,
                train='train.txt', validation='valid.txt', test='test.txt'):
-        path = '../../data/processed_simplequestions_dataset/'
         return super(SQdataset, cls).splits(
             path, '', train, validation, test,
             format='TSV', fields=[('id', None), ('sub', None), ('entity', None), ('relation', label_field),
